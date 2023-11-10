9165eb46ae240f4a6d8f77d7eb8d6bfde713aa99
Lourdes Cervera <cerveralourdes95@gmail.com>
2023-11-10 00:27:11 -0300

    routes auth y usuarios

---
+++ routes/categorias.js
@@ -0,0 +1,73 @@
+const { Router } = require("express");
+const { check } = require("express-validator");
+
+const { validarCampos } = require("../middlewares/validar-campos");
+const { validarJWT } = require("../middlewares/validar-jwt");
+const { esAdminRole, tieneRol } = require("../middlewares/validar-role");
+
+
+const { categoriaExiste } = require("../helpers/db-validators");
+
+const {
+  crearCategoria,
+  obtenerCategorias,
+  obtenerCategoria,
+  actualizarCategoria,
+  borrarCategoria,
+} = require("../controllers/categorias");
+
+const router = Router();
+
+router.get("/", obtenerCategorias);
+
+router.get(
+  "/:id",
+  [
+    check("id", "No es un id válido").isMongoId(),
+   
+    check("id").custom(categoriaExiste),
+    validarCampos,
+  ],
+  obtenerCategoria
+);
+
+router.post(
+  "/",
+  [
+    validarJWT, 
+    tieneRol("ADMIN_ROLE", "GERENTE"),
+    check("nombre", "El nombre es obligatorio").notEmpty(),
+  ],
+  crearCategoria
+);
+
+router.put(
+  "/:id",
+  [
+    validarJWT,
+    
+    esAdminRole,
+    check("id", "No es un id válido").isMongoId(),
+    
+    check("id").custom(categoriaExiste),
+    check("nombre", "El nombre es obligatorio").notEmpty(),
+    validarCampos,
+  ],
+  actualizarCategoria
+);
+
+router.delete(
+  "/:id",
+  [
+    validarJWT,
+    
+    esAdminRole,
+    check("id", "No es un id válido").isMongoId(),
+    
+    check("id").custom(categoriaExiste),
+    validarCampos,
+  ],
+  borrarCategoria
+);
+
+module.exports = router;
