095314574b99f9d437eadd996c480e26ea5121aa
Leonardo Zamorano <nvreparaciones@gmail.com>
2023-11-20 20:57:25 -0300

    se realiza 3RA correccion para deployar en FL0

--- models/server.js
+++ models/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const {dbConection} = require('../database/config');
+
 class Server {
   constructor() {
     this.app = express();
